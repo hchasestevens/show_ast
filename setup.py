362de0b0e4808c299be7b7cba9e3a9c005e835e6
H. Chase Stevens <chase@chasestevens.com>
2016-09-03 21:30:40 +0100

    Bump minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='showast',
     packages=['showast'],
-    version='0.1.0',
+    version='0.1.1',
     description = 'IPython notebook plugin for visualizing abstract syntax trees.',
     license='MIT',
     author='H. Chase Stevens',
